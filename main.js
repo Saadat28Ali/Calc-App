bedd3bc9b258a7b22e37b654726928540a0e9d46
Saadat Ali <saadat-ali@live.com>
2024-02-20 15:38:59 +0530

    quit button patched

--- main.js
+++ main.js
@@ -33,8 +33,8 @@ function createWindow(type) {
 			backgroundColor: "rgba(0, 0, 0, 0)", 
 			webPreferences: {
 				nodeIntegration: true, 
-				preload: path.join(__dirname, "page/preload.js"), 
-				devTools: false, 
+				preload: path.join(__dirname, "preload.js"), 
+				devTools: false
 			}
 		}
 		)
